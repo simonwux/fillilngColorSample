dee799e09198cc9c7123718c4c1d2a85567fd41e
John Alexis Guerra Gómez <john.guerra@gmail.com>
2019-03-01 15:19:36 -0500

    Adding users

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
 
+
+import AccountsUIWrapper from "./AccountsUIWrapper.js";
+
 export default class App extends Component {
   render() {
     return (
-      <div> Bare bones REact</div>
+      <div>
+        <div>Collaborative Painting</div>
+
+        <AccountsUIWrapper/>
+
+        <div>Made by John with ♥️</div>
+      </div>
     );
   }
 }
