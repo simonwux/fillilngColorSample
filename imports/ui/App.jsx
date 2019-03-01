4b10cc411a80993a0a32bf5ac7734ebb5442f729
John Alexis Guerra Gómez <john.guerra@gmail.com>
2019-03-01 15:10:21 -0500

    Cleaning up

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import Hello from './Hello.jsx';
-import Info from './Info.jsx';
+import React, { Component } from "react";
 
-const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <Info />
-  </div>
-);
-
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <div> Bare bones REact</div>
+    );
+  }
+}
